3decda9996b1224c3556ace391abf9b5ad431fe8
marksibly <blitzmunter@gmail.com>
2016-11-28 10:56:31 +1300

    Updated common.sh

--- scripts/common.sh
+++ scripts/common.sh
@@ -9,26 +9,26 @@ launcher_new=""
 if [ "$OSTYPE" = "linux-gnu" ]
 then
 	mx2cc="../bin/mx2cc_linux"
-	mx2cc_new="../src/mx2cc/mx2cc.buildv1.1.01/linux_release/mx2cc"
+	mx2cc_new="../src/mx2cc/mx2cc.buildv1.1.02/linux_release/mx2cc"
 	
 	ted2="../bin/ted2_linux"
-	ted2_new="../src/ted2/ted2.buildv1.1.01/linux_release"
-	ted2go_new="../src/ted2go/ted2.buildv1.1.01/linux_release"
+	ted2_new="../src/ted2/ted2.buildv1.1.02/linux_release"
+	ted2go_new="../src/ted2go/ted2.buildv1.1.02/linux_release"
 	
 	launcher="../Monkey2 (Linux)"
-	launcher_new="../src/launcher/launcher.buildv1.1.01/linux_release/launcher"
+	launcher_new="../src/launcher/launcher.buildv1.1.02/linux_release/launcher"
 	
 elif [ "$OSTYPE" = "linux-gnueabihf" ]
 then
 	mx2cc="../bin/mx2cc_raspbian"
-	mx2cc_new="../src/mx2cc/mx2cc.buildv1.0.9/raspbian_release/mx2cc"
+	mx2cc_new="../src/mx2cc/mx2cc.buildv1.1.02/raspbian_release/mx2cc"
 	
 	ted2="../bin/ted2_raspbian"
-	ted2_new="../src/ted2/ted2.buildv1.0.9/raspbian_release"
-	ted2go_new="../src/ted2go/ted2.buildv1.0.9/raspbian_release"
+	ted2_new="../src/ted2/ted2.buildv1.1.02/raspbian_release"
+	ted2go_new="../src/ted2go/ted2.buildv1.1.02/raspbian_release"
 	
 	launcher="../Monkey2 (Raspbian)"
-	launcher_new="../src/launcher/launcher.buildv1.0.9/raspbian_release/launcher"
+	launcher_new="../src/launcher/launcher.buildv1.1.02/raspbian_release/launcher"
 else
 	mx2cc="../bin/mx2cc_macos"
 	mx2cc_new="../src/mx2cc/mx2cc.buildv1.1.02/macos_release/mx2cc"
