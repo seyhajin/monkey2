6ba8cc6329a3603295e7307ee9e9c54d05481146
Mark Sibly <blitzmunter@gmail.com>
2017-07-27 10:59:43 +1200

    Cleanups.

--- scripts/updatemx2cc.sh
+++ scripts/updatemx2cc.sh
@@ -5,7 +5,7 @@ echo ""
 echo "***** Updating mx2cc *****"
 echo ""
 
-$mx2cc makemods -config=release
+$mx2cc makemods -config=release monkey libc miniz stb-image stb-image-write stb-vorbis std
 
 $mx2cc makeapp -apptype=console -config=release ../src/mx2cc/mx2cc.monkey2
 
