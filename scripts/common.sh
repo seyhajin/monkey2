f1c60d093a28ba62955e3ceadadcfa3f8459c84e
Mark Sibly <blitzmunter@gmail.com>
2017-07-27 14:53:08 +1200

    Updated common.sh to 1.1.06

--- scripts/common.sh
+++ scripts/common.sh
@@ -9,34 +9,34 @@ launcher_new=""
 if [ "$OSTYPE" = "linux-gnu" ]
 then
 	mx2cc="../bin/mx2cc_linux"
-	mx2cc_new="../src/mx2cc/mx2cc.buildv1.1.05/linux_release/mx2cc"
+	mx2cc_new="../src/mx2cc/mx2cc.buildv1.1.06/linux_release/mx2cc"
 	
 	ted2="../bin/ted2_linux"
-	ted2_new="../src/ted2/ted2.buildv1.1.05/linux_release"
-	ted2go_new="../src/ted2go/Ted2.buildv1.1.05/linux_release"
+	ted2_new="../src/ted2/ted2.buildv1.1.06/linux_release"
+	ted2go_new="../src/ted2go/Ted2.buildv1.1.06/linux_release"
 	
 	launcher="../Monkey2 (Linux)"
-	launcher_new="../src/launcher/launcher.buildv1.1.05/linux_release/launcher"
+	launcher_new="../src/launcher/launcher.buildv1.1.06/linux_release/launcher"
 	
 elif [ "$OSTYPE" = "linux-gnueabihf" ]
 then
 	mx2cc="../bin/mx2cc_raspbian"
-	mx2cc_new="../src/mx2cc/mx2cc.buildv1.1.02/raspbian_release/mx2cc"
+	mx2cc_new="../src/mx2cc/mx2cc.buildv1.1.06/raspbian_release/mx2cc"
 	
 	ted2="../bin/ted2_raspbian"
-	ted2_new="../src/ted2/ted2.buildv1.1.02/raspbian_release"
-	ted2go_new="../src/ted2go/Ted2.buildv1.1.02/raspbian_release"
+	ted2_new="../src/ted2/ted2.buildv1.1.06/raspbian_release"
+	ted2go_new="../src/ted2go/Ted2.buildv1.1.06/raspbian_release"
 	
 	launcher="../Monkey2 (Raspbian)"
-	launcher_new="../src/launcher/launcher.buildv1.1.02/raspbian_release/launcher"
+	launcher_new="../src/launcher/launcher.buildv1.1.06/raspbian_release/launcher"
 else
 	mx2cc="../bin/mx2cc_macos"
-	mx2cc_new="../src/mx2cc/mx2cc.buildv1.1.05/macos_release/mx2cc"
+	mx2cc_new="../src/mx2cc/mx2cc.buildv1.1.06/macos_release/mx2cc"
 	
 	ted2="../bin/ted2_macos.app"
-	ted2_new="../src/ted2/ted2.buildv1.1.05/macos_release/ted2.app"
-	ted2go_new="../src/ted2go/Ted2.buildv1.1.05/macos_release/ted2.app"
+	ted2_new="../src/ted2/ted2.buildv1.1.06/macos_release/ted2.app"
+	ted2go_new="../src/ted2go/Ted2.buildv1.1.06/macos_release/ted2.app"
 	
 	launcher="../Monkey2 (Macos).app"
-	launcher_new="../src/launcher/launcher.buildv1.1.05/macos_release/launcher.app"
+	launcher_new="../src/launcher/launcher.buildv1.1.06/macos_release/launcher.app"
 fi
