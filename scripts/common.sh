6274116eb1166af93d5c6c502648a3a109a8a126
Mark Sibly <blitzmunter@gmail.com>
2017-09-21 07:34:55 +1200

    Updated.

--- scripts/common.sh
+++ scripts/common.sh
@@ -8,35 +8,25 @@ launcher_new=""
 
 if [ "$OSTYPE" = "linux-gnu" ]
 then
+
 	host="linux"
 	mx2cc="../bin/mx2cc_linux"
-	
 	ted2="../bin/ted2_linux"
-	ted2_new="../src/ted2/ted2.buildv1.1.07/linux_release"
-	ted2go_new="../src/ted2go/Ted2.buildv1.1.07/linux_release"
-	
 	launcher="../Monkey2 (Linux)"
-	launcher_new="../src/launcher/launcher.buildv1.1.07/linux_release/launcher"
 	
 elif [ "$OSTYPE" = "linux-gnueabihf" ]
 then
+
 	host="raspbian"
 	mx2cc="../bin/mx2cc_raspbian"
-	
 	ted2="../bin/ted2_raspbian"
-	ted2_new="../src/ted2/ted2.buildv1.1.06/raspbian_release"
-	ted2go_new="../src/ted2go/Ted2.buildv1.1.06/raspbian_release"
-	
 	launcher="../Monkey2 (Raspbian)"
-	launcher_new="../src/launcher/launcher.buildv1.1.06/raspbian_release/launcher"
+	
 else
+
 	host="macos"
 	mx2cc="../bin/mx2cc_macos"
-	
 	ted2="../bin/ted2_macos.app"
-	ted2_new="../src/ted2/ted2.buildv1.1.07/macos_release/ted2.app"
-	ted2go_new="../src/ted2go/Ted2.buildv1.1.07/macos_release/Ted2.app"
-	
 	launcher="../Monkey2 (Macos).app"
-	launcher_new="../src/launcher/launcher.buildv1.1.07/macos_release/launcher.app"
+	
 fi
--- scripts/updateted2go.sh
+++ scripts/updateted2go.sh
@@ -2,7 +2,7 @@
 source common.sh
 
 echo ""
-echo "***** Updating ted2 *****"
+echo "***** Updating ted2go *****"
 echo ""
 
 $mx2cc makeapp -apptype=gui -build -config=release -product=scripts/ted2go.products/$host/ted2 ../src/ted2go/Ted2.monkey2
@@ -14,26 +14,27 @@ then
 
 	rm -r -f $ted2
 	mkdir $ted2
-	cp -R ./ted2go.products/linux/assets $ted2/assets
-	cp ./ted2go.products/linux/ted2 $ted2/ted2
+	cp -R ./ted2go.products/$host/assets $ted2/assets
+	cp ./ted2go.products/$host/ted2 $ted2/ted2
 	
 	rm -r -f "$launcher"
-	cp ./launcher.products/linux/launcher "$launcher"
+	cp ./launcher.products/$host/launcher "$launcher"
 
 elif [ "$OSTYPE" = "linux-gnueabihf" ]
 then
 
-	rm -r -f "$ted2"
-	mkdir "$ted2"
-	cp -R "$ted2go_new/assets" "$ted2/assets"
-	cp "$ted2go_new/Ted2" "$ted2/ted2"
+	rm -r -f $ted2
+	mkdir $ted2
+	cp -R ./ted2go.products/$host/assets $ted2/assets
+	cp ./ted2go.products/$host/ted2 $ted2/ted2
+	
 	rm -r -f "$launcher"
-	cp "$launcher_new" "$launcher"
+	cp ./launcher.products/$host/launcher "$launcher"
 
 else
 
 	rm -r -f $ted2
-	cp -R ./ted2go.products/macos/Ted2.app $ted2
+	cp -R ./ted2go.products/macos/ted2.app $ted2
 	
 	rm -r -f "$launcher"
 	cp -R ./launcher.products/macos/Launcher.app "$launcher"
