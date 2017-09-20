9fb9404b36c97f035c166f2ecbc88d4f7c134658
Mark Sibly <blitzmunter@gmail.com>
2017-09-21 06:26:30 +1200

    Updated.

--- scripts/rebuildmx2cc.sh
+++ scripts/rebuildmx2cc.sh
@@ -5,6 +5,8 @@ echo ""
 echo "***** Rebuilding mx2cc *****"
 echo ""
 
+mkdir mx2cc.products
+
 $mx2cc makemods -clean -config=release monkey libc miniz stb-image stb-image-write stb-vorbis std
 
 $mx2cc makeapp -clean -apptype=console -config=release -product=scripts/mx2cc.products/mx2cc_$host ../src/mx2cc/mx2cc.monkey2
