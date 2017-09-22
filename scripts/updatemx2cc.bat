5723ef3ae6b5bc4849cc9ce77053dfb47678483b
Mark Sibly <blitzmunter@gmail.com>
2017-09-22 22:24:14 +1200

    updates.

--- scripts/updatemx2cc.bat
+++ scripts/updatemx2cc.bat
@@ -7,6 +7,8 @@ echo.
 echo ***** Updating mx2cc *****
 echo.
 
+mkdir mx2cc.products
+
 %mx2cc% makemods -config=release monkey libc miniz stb-image stb-image-write stb-vorbis std
 
 %mx2cc% makeapp -apptype=console -config=release -product=scripts/mx2cc.products/mx2cc_windows.exe ../src/mx2cc/mx2cc.monkey2
