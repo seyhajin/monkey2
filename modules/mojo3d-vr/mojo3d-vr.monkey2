912be4192dcd828462ce795248bb5915fbc14dac
Mark Sibly <blitzmunter@gmail.com>
2018-02-07 11:12:35 +1300

    Added mojo3d-vr module.

---
+++ modules/mojo3d-vr/mojo3d-vr.monkey2
@@ -0,0 +1,20 @@
+
+namespace mojo3d
+
+#If __DESKTOP_TARGET__
+
+#Import "<std>"
+#Import "<mojo>"
+#Import "<mojo3d>"
+
+Using std..
+Using mojo..
+Using mojo3d..
+Using openvr..
+
+#Import "openvr/openvr-sdk/openvr"
+
+#Import "openvr/vrrenderer"
+#Import "openvr/geomtrans"
+
+#end
---
+++ modules/mojo3d-vr/openvr/openvr-sdk/glue.h
@@ -0,0 +1,23 @@
+
+#if 0 //_MSC_VER
+
+#include <openvr.h>
+
+#else
+//
+// This address an issue with differences between the msvc/mingw abis when returning structs.
+//
+// See:
+//
+// https://github.com/ValveSoftware/openvr/issues/133
+//
+// Looks like the GCC guys don't quite get it yet, still think it's caller/callee popping issue:
+//
+// https://gcc.gnu.org/bugzilla/show_bug.cgi?id=64384
+
+#include <openvr_mingw.h>
+
+#endif
+
+
+using namespace vr;
