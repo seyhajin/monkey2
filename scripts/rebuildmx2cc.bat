361187ff4986daee50359943020130798cd2db8e
Mark Sibly <blitzmunter@gmail.com>
2018-04-22 11:01:43 +1200

    Added zlib to mx2cc build scripts.

--- scripts/rebuildmx2cc.bat
+++ scripts/rebuildmx2cc.bat
@@ -7,7 +7,7 @@ echo.
 echo ***** Rebuilding mx2cc *****
 echo.
 
-%mx2cc% makemods -clean -config=release monkey libc miniz stb-image stb-image-write stb-vorbis std
+%mx2cc% makemods -clean -config=release monkey libc miniz stb-image stb-image-write stb-vorbis zlib std
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 %mx2cc% makeapp -clean -apptype=console -config=release -product=scripts/mx2cc.products/mx2cc_windows.exe ../src/mx2cc/mx2cc.monkey2
