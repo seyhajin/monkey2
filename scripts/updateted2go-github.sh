66df43ea75f9446c7f04b220f5408ac8ffe512ff
Mark Sibly <blitzmunter@gmail.com>
2018-07-21 09:25:17 +1200

    Added new script file to build ted2go-github on unix.

---
+++ scripts/updateted2go-github.sh
@@ -0,0 +1,45 @@
+
+source common.sh
+
+echo ""
+echo "***** Updating ted2go *****"
+echo ""
+
+$mx2cc makeapp -apptype=gui -build -config=release -product=scripts/ted2go-github.products/$host/ted2 ../src/ted2go-github/Ted2.monkey2
+
+$mx2cc makeapp -apptype=gui -build -config=release -product=scripts/launcher.products/$host/launcher ../src/launcher/launcher.monkey2
+
+if [ "$OSTYPE" = "linux-gnu" ]
+then
+
+	rm -r -f $ted2
+	mkdir $ted2
+	cp -R ./ted2go-github.products/$host/assets $ted2/assets
+	cp ./ted2go-github.products/$host/ted2 $ted2/ted2
+	
+	rm -r -f "$launcher"
+	cp ./launcher.products/$host/launcher "$launcher"
+
+elif [ "$OSTYPE" = "linux-gnueabihf" ]
+then
+
+	rm -r -f $ted2
+	mkdir $ted2
+	cp -R ./ted2go-github.products/$host/assets $ted2/assets
+	cp ./ted2go-github.products/$host/ted2 $ted2/ted2
+	
+	rm -r -f "$launcher"
+	cp ./launcher.products/$host/launcher "$launcher"
+
+else
+
+	rm -r -f $ted2
+	cp -R ./ted2go-github.products/macos/ted2.app $ted2
+	
+	rm -r -f "$launcher"
+	cp -R ./launcher.products/macos/Launcher.app "$launcher"
+	
+	cp ../src/launcher/info.plist "$launcher/Contents"
+	cp ../src/launcher/Monkey2logo.icns "$launcher/Contents/Resources"
+
+fi
