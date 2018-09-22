cf89d1016d069a4e5923151ecedeaac4099820e5
Mark Sibly <blitzmunter@gmail.com>
2018-09-22 13:23:04 +1200

    WIP fixing dodgy NSString conversions.

--- modules/monkey/native/bbstring.mm
+++ modules/monkey/native/bbstring.mm
@@ -1,10 +1,13 @@
 
 #include "bbstring.h"
 
-bbString::bbString( const NSString *str ):_rep( Rep::create( str ? str.UTF8String : "" ) ){
+//bbString::bbString( const NSString *str ):_rep( Rep::create( str ? str.UTF8String : "" ) ){
+bbString::bbString( const NSString *str ):_rep( Rep::alloc( [str.length] ) ){
+	[str getCharacters:(unichar*)data() range:{0,length()}];
 }
 
 NSString *bbString::ToNSString()const{
 
-	return [NSString stringWithUTF8String:c_str()];
+//	return [NSString stringWithUTF8String:c_str()];
+	return [NSString stringWithCharacters:(unichar*)data() length:length()];
 }
