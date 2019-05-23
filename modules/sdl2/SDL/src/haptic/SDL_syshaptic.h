9c9ae7a1a1edff9f13f1b5be1a435cc9cded0440
seyhajin <seyhajin@gmail.com>
2019-05-23 15:19:52 +0200

    Update latest SDL2 with original headers/sources from version 2.0.9.

--- modules/sdl2/SDL/src/haptic/SDL_syshaptic.h
+++ modules/sdl2/SDL/src/haptic/SDL_syshaptic.h
@@ -1,6 +1,6 @@
 /*
   Simple DirectMedia Layer
-  Copyright (C) 1997-2016 Sam Lantinga <slouken@libsdl.org>
+  Copyright (C) 1997-2018 Sam Lantinga <slouken@libsdl.org>
 
   This software is provided 'as-is', without any express or implied
   warranty.  In no event will the authors be held liable for any damages
@@ -21,8 +21,8 @@
 
 #include "../SDL_internal.h"
 
-#ifndef _SDL_syshaptic_h
-#define _SDL_syshaptic_h
+#ifndef SDL_syshaptic_h_
+#define SDL_syshaptic_h_
 
 #include "SDL_haptic.h"
 
@@ -203,6 +203,6 @@ extern int SDL_SYS_HapticUnpause(SDL_Haptic * haptic);
  */
 extern int SDL_SYS_HapticStopAll(SDL_Haptic * haptic);
 
-#endif /* _SDL_syshaptic_h */
+#endif /* SDL_syshaptic_h_ */
 
 /* vi: set ts=4 sw=4 expandtab: */
